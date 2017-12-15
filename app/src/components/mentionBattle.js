ae6fc2c86c9c3bee5c1950db017113e7e4eb584c
Vladislav Kamenev <mazahakaforever@ya.ru>
2017-12-16 00:25:37 +0300

    Planes. Planes are now limited to 200ms.

--- app/src/components/mentionBattle.js
+++ app/src/components/mentionBattle.js
@@ -12,10 +12,16 @@
             templateUrl: 'mention-battle.tpl.html',
             controllerAs: 'vm',
             controller: function ($scope, $element, events, $timeout) {
-                var vm = this;
                 var target = document.querySelector('mention-battle').querySelector('.container');
+                var lastPlane = Date.now();
 
-                $scope.$on(events.NEW_MENTION, function ($event, contenderNumber) {
+                $scope.$on(events.NEW_MENTION, makePlane);
+
+                function makePlane($event, contenderNumber) {
+                    if (Date.now() - lastPlane < 200) {
+                        return;
+                    }
+
                     var newPlane = angular.element(contenderNumber === 1 ? planeRed : planeBlue);
                     angular.element(target).append(newPlane);
                     $timeout(function () {
@@ -24,7 +30,8 @@
                     $timeout(function () {
                         newPlane.remove();
                     }, planeDomElementDieInSeconds * 1000);
-                });
+                    lastPlane = Date.now();
+                }
             }
         });
 })();
